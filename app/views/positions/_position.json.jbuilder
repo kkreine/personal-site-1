6f76e12824d24f00d7698f293517fa5532f67735
Kori Reine <kkr.reine@gmail.com>
2021-10-21 15:08:29 -0500

    added pg gem

--- app/views/positions/_position.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! position, :id, :title, :company, :description, :location, :dates, :created_at, :updated_at
-json.url position_url(position, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :positions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   get 'resume', to: 'pages#resume'
